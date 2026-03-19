ff1de912599664db716496a602267244c9c956df
Rudy <duyr190@gmail.com>
2026-03-19 11:05:11 -0400

    test

--- build/src/main.c
+++ build/src/main.c
@@ -9,5 +9,6 @@ int main(void){
   char* test = "test";
   char test2[] =  "Test2";
   char test3[] = {'1', '2', '3', '\0'};
-  printf("Yes %s %s %s", test, test2, test3);
+  char* test4 [] = {"1234", "5678"};
+  printf("Yes %s %s %s %s", test, test2, test3, test4[0]);
 }

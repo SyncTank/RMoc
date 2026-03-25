0bbc888775d7584007bcb8f18bad5ae104a185a9
Rudy <duyr190@gmail.com>
2026-03-25 13:33:53 -0400

    file r/w

--- build/src/main.c
+++ build/src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>	
+#include <stdlib.h>
 
 #define STR_IMPL
 #include "string_block.h"
@@ -10,10 +11,24 @@ typedef struct StringDynamic
   size_t capacity;
 } DynString; 
 
+void r_w_file()
+{
+  FILE *in_file  = fopen("name_of_file", "r"); // read only
+  FILE *out_file = fopen("name_of_file", "w"); // write only
+  if (in_file == NULL || out_file == NULL)
+  {
+    printf("Error! Could not open file\n");
+    exit(-1); 
+  }
+}
 
 int main(int argc, char* argv[]){
 
   printf("Arg count : %i\nArgs pointer %p first item : %s\n", argc, *argv, argv[0]);
   string_test();
+
+  char str[50];
+  scanf("%s", str);
+  printf("%s\n", str);
 
 }
